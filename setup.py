77561127fb4a48234ea49fc6464624480a26870a
abidibo <abidibo@gmail.com>
2017-02-11 15:34:09 +0100

    pip and setup files

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-baton',
+    version='0.1.0-rc1',
+    packages=['baton', 'baton.autodiscover', 'baton.templatetags'],
+    include_package_data=True,
+    license='MIT License',
+    description='A cool, modern and responsive django admin application',
+    long_description=README,
+    url='http://github.com/otto-torino/django-baton',
+    author='abidibo',
+    author_email='abidibo@gmail.com',
+    install_requires=[
+        'Django',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+    ]
+)
