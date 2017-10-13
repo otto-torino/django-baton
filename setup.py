dd4527170e6968eb2fe9c901ada3f9a85dbb1c3c
abidibo <abidibo@gmail.com>
2017-10-13 11:05:14 +0200

    bumped version number to 1.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-baton',
-    version='1.0.4',
+    version='1.0.5',
     packages=['baton', 'baton.autodiscover', 'baton.templatetags'],
     include_package_data=True,
     license='MIT License',
