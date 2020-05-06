87097c767dcf07db7035c6484c55ccc90698c9fd
Tom Anthony <git@tomanthony.co.uk>
2020-05-06 10:43:01 +0100

    Fix 'active menu item' regex match on 'free' menu items (URLs).

--- baton/static/baton/app/src/core/Menu.js
+++ baton/static/baton/app/src/core/Menu.js
@@ -54,11 +54,7 @@ let Menu = {
     let self = this
     let mainUl = $('<ul/>', { 'class': 'depth-0' }).appendTo(self.menu)
     data.forEach((voice, index) => {
-      let active = false
-      if (voice.url) {
-        let pathRexp = new RegExp(voice.url)
-        active = pathRexp.test(location.pathname)
-      }
+      let active = (location.pathname == voice.url)
       let li = $('<li />', { 'class': 'top-level ' + voice.type + (active ? ' active' : '') })
       let a = $('<' + (voice.url ? 'a' : 'span') + ' />', {
         href: voice.url || '#'
