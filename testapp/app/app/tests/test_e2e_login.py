d64723f66912889c15d86f3acf543d669ea79e69
abidibo <abidibo@gmail.com>
2020-05-26 09:45:59 +0200

    adds first e2e test

---
+++ testapp/app/app/tests/test_e2e_login.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from django.test import TestCase
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class TestBatonLogin(TestCase):
+    def setUp(self):
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(
+            options=chrome_options, executable_path=os.getenv('CHROME_DRIVER_PATH'))
+
+    def test_form(self):
+        self.driver.get('http://localhost:8000/admin')
+        header_field = self.driver.find_element_by_id("header")
+        self.assertEqual(header_field.text, 'Baton Test App')
