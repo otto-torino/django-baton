11eefa016aa67a7f1679a23e47a41e8dead785c7
abidibo <abidibo@gmail.com>
2020-06-22 14:49:52 +0200

    bumps version number to 1.8.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-baton',
-    version='1.8.2',
+    version='1.8.3',
     packages=['baton', 'baton.autodiscover', 'baton.templatetags'],
     include_package_data=True,
     license='MIT License',
