e84aaf3a9a578dd61c24248070270a67fdae822a
abidibo <abidibo@gmail.com>
2021-05-31 18:53:01 +0200

    Changes some text-overflow values

--- baton/static/baton/app/src/index.js
+++ baton/static/baton/app/src/index.js
@@ -18,8 +18,6 @@ import Filer from './core/Filer'
 import Modal from './core/Modal'
 import Messages from './core/Messages'
 
-console.log('DIOMERDA')
-
 window.Baton = {
   intialized: false,
   init: function (config) {
@@ -107,5 +105,5 @@ window.Baton = {
   Dispatcher: Dispatcher,
   Modal: Modal
 }
-console.log('DIOCAZZO', jQuery)
+
 window.jQuery = jQuery
