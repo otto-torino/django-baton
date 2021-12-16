54fbb08bad63106531ff1dd21761c94c75d97825
abidibo <abidibo@gmail.com>
2021-12-16 12:05:15 +0100

    Add django 4 support

--- baton/urls.py
+++ baton/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import GetAppListJsonView, GetGravatartUrlJsonView
 
 
 urlpatterns = [
-    url(r'^app-list-json/$', GetAppListJsonView.as_view(),
+    path('app-list-json/', GetAppListJsonView.as_view(),
         name='baton-app-list-json'),
-    url(r'^gravatar/$', GetGravatartUrlJsonView.as_view(),
+    path('gravatar/', GetGravatartUrlJsonView.as_view(),
         name='baton-gravatar-json'),
 ]
