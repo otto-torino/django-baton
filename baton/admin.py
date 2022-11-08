d9d77806a15743422e8d1b30bd5abf57824db41f
abidibo <abidibo@gmail.com>
2022-11-08 10:37:04 +0100

    Implement #170: add multiple choice list filter

--- baton/admin.py
+++ baton/admin.py
@@ -44,3 +44,54 @@ class RelatedDropdownFilter(RelatedFieldListFilter):
 
 class RelatedOnlyDropdownFilter(RelatedOnlyFieldListFilter):
     template = 'baton/filters/dropdown_filter.html'
+
+
+class MultipleChoiceListFilter(admin.SimpleListFilter):
+    template = 'baton/filters/multiple_choice_filter.html'
+
+    def lookups(self, request, model_admin):
+        """
+        Must be overridden to return a list of tuples (value, verbose value)
+        """
+        raise NotImplementedError(
+            'The MultipleChoiceListFilter.lookups() method must be overridden to '
+            'return a list of tuples (value, verbose value).'
+        )
+
+    def queryset(self, request, queryset):
+        if request.GET.get(self.parameter_name):
+            kwargs = {self.parameter_name: request.GET[self.parameter_name].split(',')}
+            queryset = queryset.filter(**kwargs)
+        return queryset
+
+    def value_as_list(self):
+        return self.value().split(',') if self.value() else []
+
+    def choices(self, changelist):
+
+        def amend_query_string(include=None, exclude=None):
+            selections = self.value_as_list()
+            if include and include not in selections:
+                selections.append(include)
+            if exclude and exclude in selections:
+                selections.remove(exclude)
+            if selections:
+                csv = ','.join(selections)
+                return changelist.get_query_string({self.parameter_name: csv})
+            else:
+                return changelist.get_query_string(remove=[self.parameter_name])
+
+        yield {
+            'selected': self.value() is None,
+            'query_string': changelist.get_query_string(remove=[self.parameter_name]),
+            'display': 'All',
+            'reset': True,
+        }
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': str(lookup) in self.value_as_list(),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                'include_query_string': amend_query_string(include=str(lookup)),
+                'exclude_query_string': amend_query_string(exclude=str(lookup)),
+                'display': title,
+            }
