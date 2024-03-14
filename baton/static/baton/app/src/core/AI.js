b9cab5bbf3ba1e83733589b1a4873b89e2a1d047
abidibo <abidibo@gmail.com>
2024-03-14 21:23:06 +0100

    feat: change diff type

--- baton/static/baton/app/src/core/AI.js
+++ baton/static/baton/app/src/core/AI.js
@@ -379,20 +379,20 @@ const AI = {
           $(field).after(checkIcon)
         } else if (data?.data?.text) {
           const decodedText = $('<textarea />').html(text).text()
-          const diff = Diff.diffChars(decodedText, data?.data?.text)
-          const fragment = $('<div />')
+          const diff = Diff.diffWords(decodedText, data?.data?.text)
+          // const fragment = $('<div />')
 
           const diffParts = []
           diff.forEach((part) => {
             // green for additions, red for deletions
             // grey for common parts
-            const color = part.added ? 'green' : part.removed ? 'red' : 'black'
-            const fontWeight = part.added ? '700' : part.removed ? '700' : '400'
-            const span = $('<span />').css({ color: color, fontWeight: fontWeight }).text(part.value)
-            fragment.append(span)
-            diffParts.push(part.added ? `<span style="color: green">${part.value}</span>` : part.removed ? `<span style="color: red">${part.value}</span>` : `${part.value}`)
+            // const color = part.added ? 'green' : part.removed ? 'red' : 'black'
+            // const fontWeight = part.added ? '700' : part.removed ? '700' : '400'
+            // const span = $('<span />').css({ color: color, fontWeight: fontWeight }).text(part.value)
+            // fragment.append(span)
+            diffParts.push(part.added ? `<span style="color: green; background: rgba(0, 255, 0, 0.2)">${part.value}</span>` : part.removed ? `<span style="color: red; background: rgba(255, 0, 0, 0.2)">${part.value}</span>` : `${part.value}`)
           })
-          const fragmentHtml = fragment[0].outerHTML
+          // const fragmentHtml = fragment[0].outerHTML
           const content = `
 <div class="row">
 <div class="col-md-4">
