9fdc7136e674f20d78723f6a80f37e2568746c14
abidibo <abidibo@gmail.com>
2025-05-21 17:34:48 +0200

    test: fix login test

--- testapp/app/app/tests/test_e2e_login.py
+++ testapp/app/app/tests/test_e2e_login.py
@@ -9,7 +9,7 @@ from testapp.app.app.tests.utils import make_driver
 
 import os
 
-os.environ['WDM_LOG_LEVEL'] = '0'
+os.environ["WDM_LOG_LEVEL"] = "0"
 
 
 class TestBatonLogin(TestCase):
@@ -20,29 +20,29 @@ class TestBatonLogin(TestCase):
         self.driver.quit()
 
     def test_form(self):
-        self.driver.get('http://localhost:8000/admin')
+        self.driver.get("http://localhost:8000/admin")
         # wait for page to load
         try:
-            element_present = EC.presence_of_element_located(
-                (By.ID, 'header'))
+            element_present = EC.presence_of_element_located((By.ID, "header"))
             WebDriverWait(self.driver, 10).until(element_present)
         except TimeoutException:
             print("Timed out waiting for page to load")
 
         time.sleep(1)
         header_field = self.driver.find_element(By.ID, "header")
-        self.assertEqual(header_field.text, 'Baton Test App')
+        self.assertEqual(header_field.text, "Baton Test App")
 
         username_field = self.driver.find_element(By.ID, "id_username")
         password_field = self.driver.find_element(By.ID, "id_password")
-        button = self.driver.find_element(By.CSS_SELECTOR, 'input[type=submit]')
+        button = self.driver.find_element(By.CSS_SELECTOR, "input[type=submit]")
         self.assertEqual(password_field.is_displayed(), True)
         self.assertEqual(username_field.is_displayed(), True)
         self.assertEqual(button.is_displayed(), True)
 
-        username_field.send_keys('admin')
+        username_field.send_keys("admin")
         time.sleep(1)
-        password_field.send_keys('admin')
+        password_field.send_keys("admin")
         time.sleep(1)
         button.click()
-        self.assertEqual(self.driver.current_url, 'http://localhost:8000/admin/')
+        time.sleep(1)
+        self.assertEqual(self.driver.current_url, "http://localhost:8000/admin/")
