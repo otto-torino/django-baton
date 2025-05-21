21b67feb7f909b7b5aad8a9e25ae893d1956a6d1
abidibo <abidibo@gmail.com>
2025-05-21 17:16:48 +0200

    fix: inlines collapse-entry and hidden details issue

--- testapp/app/news/admin.py
+++ testapp/app/news/admin.py
@@ -20,24 +20,24 @@ class NewsResources(resources.ModelResource):
 
 
 class TitleFilter(InputFilter):
-    parameter_name = 'title'
-    title = 'title'
+    parameter_name = "title"
+    title = "title"
 
     def queryset(self, request, queryset):
         if self.value() is not None:
             search_term = self.value()
-            return queryset.filter(
-                title__icontains=search_term
-            )
+            return queryset.filter(title__icontains=search_term)
+
 
 class CategoryFilter(AutocompleteFilter):
-    title = 'category' # display title
-    field_name = 'category' # name of the foreign key field
+    title = "category"  # display title
+    field_name = "category"  # name of the foreign key field
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    search_fields = ('name', )
+    list_display = ("name",)
+    search_fields = ("name",)
 
 
 class AttachmentsInline(admin.TabularInline):
@@ -48,14 +48,26 @@ class AttachmentsInline(admin.TabularInline):
 class VideosInline(admin.StackedInline):
     model = Video
     extra = 1
-    classes = ('collapse-entry', 'expand-first', )
+    classes = (
+        "collapse-entry",
+        "expand-first",
+    )
     fieldsets = (
-        ('Code', {
-            'fields': ('code', ),
-        }),
-        ('Meta', {
-            'fields': ('caption', 'author_email', ),
-        })
+        (
+            "Code",
+            {
+                "fields": ("code",),
+            },
+        ),
+        (
+            "Meta",
+            {
+                "fields": (
+                    "caption",
+                    "author_email",
+                ),
+            },
+        ),
     )
 
 
@@ -63,87 +75,155 @@ class ActivitiesInline(GenericStackedInline):
     model = Activity
     form = ActivityForm
     extra = 1
+    classes = (
+        "collapse-entry",
+        "expand-first",
+    )
 
 
 class StatusListFilter(MultipleChoiceListFilter):
-    title = 'Status'
-    parameter_name = 'status__in'
+    title = "Status"
+    parameter_name = "status__in"
 
     def lookups(self, request, model_admin):
         return News.Status.choices
+
 
 @admin.register(News)
 class NewsAdmin(ImportExportModelAdmin, TranslationAdmin):
     list_per_page = 2
     list_display = (
-        'title',
-        'date',
-        'get_category',
-        'status',
-        'published',
+        "title",
+        "date",
+        "get_category",
+        "status",
+        "published",
     )
     list_filter = (
         TitleFilter,
         CategoryFilter,
-        ('date', DateRangeFilter),
+        ("date", DateRangeFilter),
         StatusListFilter,
-        'published',
+        "published",
     )
-    search_fields = ('title', )
-    autocomplete_fields = ('category', )
-    inlines = [AttachmentsInline, VideosInline, ActivitiesInline, ]
-    date_hierarchy = 'date'
+    search_fields = ("title",)
+    autocomplete_fields = ("category",)
+    inlines = [
+        AttachmentsInline,
+        VideosInline,
+        ActivitiesInline,
+    ]
+    date_hierarchy = "date"
 
     fieldsets = (
-        ('Dates', {
-            'fields': ('date', 'datetime', ),
-            'classes': ('order-1', 'baton-tabs-init', 'baton-tab-fs-content', 'baton-tab-fs-flags', 'baton-tab-group-fs-attachments--inline-attachments', 'baton-tab-group-fs-videos--inline-videos', 'baton-tab-inline-news-activity-content_type-object_id'),
-            'description': 'This is a description text'
-
-        }),
-        ('Main', {
-            'fields': (('category', 'title'), 'link', 'content', 'summary', 'status', ),
-            'classes': ('tab-fs-content', ),
-            'description': 'This is a description text'
-
-        }),
-        ('Media', {
-            'fields': ('image', 'image_alt', 'image_subject_location', ),
-            'classes': ('collapse', ),
-        }),
-        ('Flags', {
-            'fields': ('share', 'published', ),
-            'classes': ('tab-fs-flags', ),
-            'description': 'Set sharing and publishing options'
-
-        }),
-        ('Attachments', {
-            'fields': ('attachments_summary', ),
-            'classes': ('tab-fs-attachments', ),
-            'description': 'Add as many attachments as you want'
-        }),
-        ('Videos', {
-            'fields': ('videos_summary', ),
-            'classes': ('tab-fs-videos', ),
-            'description': 'Add as many videos as you want'
-
-        }),
+        (
+            "Dates",
+            {
+                "fields": (
+                    "date",
+                    "datetime",
+                ),
+                "classes": (
+                    "order-1",
+                    "baton-tabs-init",
+                    "baton-tab-fs-content",
+                    "baton-tab-fs-flags",
+                    "baton-tab-group-fs-attachments--inline-attachments",
+                    "baton-tab-group-fs-videos--inline-videos",
+                    "baton-tab-inline-news-activity-content_type-object_id",
+                ),
+                "description": "This is a description text",
+            },
+        ),
+        (
+            "Main",
+            {
+                "fields": (
+                    ("category", "title"),
+                    "link",
+                    "content",
+                    "summary",
+                    "status",
+                ),
+                "classes": ("tab-fs-content",),
+                "description": "This is a description text",
+            },
+        ),
+        (
+            "Media",
+            {
+                "fields": (
+                    "image",
+                    "image_alt",
+                    "image_subject_location",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Flags",
+            {
+                "fields": (
+                    "share",
+                    "published",
+                ),
+                "classes": ("tab-fs-flags",),
+                "description": "Set sharing and publishing options",
+            },
+        ),
+        (
+            "Attachments",
+            {
+                "fields": ("attachments_summary",),
+                "classes": ("tab-fs-attachments",),
+                "description": "Add as many attachments as you want",
+            },
+        ),
+        (
+            "Videos",
+            {
+                "fields": ("videos_summary",),
+                "classes": ("tab-fs-videos",),
+                "description": "Add as many videos as you want",
+            },
+        ),
     )
 
     baton_form_includes = [
-        ('news/admin_datetime_include.html', 'datetime', 'top', ),
-        ('news/admin_content_include.html', 'content', 'above', ),
-        ('news/admin_title_include.html', 'title', 'right', ),
+        (
+            "news/admin_datetime_include.html",
+            "datetime",
+            "top",
+        ),
+        (
+            "news/admin_content_include.html",
+            "content",
+            "above",
+        ),
+        (
+            "news/admin_title_include.html",
+            "title",
+            "right",
+        ),
     ]
 
-    baton_form_object_tools_include = ('news/object_tools_include.html', 'left', )
+    baton_form_object_tools_include = (
+        "news/object_tools_include.html",
+        "left",
+    )
 
     baton_cl_includes = [
-        ('news/admin_cl_top_include.html', 'top', ),
+        (
+            "news/admin_cl_top_include.html",
+            "top",
+        ),
     ]
 
     baton_cl_filters_includes = [
-        ('news/admin_cl_filters_top_include.html', 'top', ),
+        (
+            "news/admin_cl_filters_top_include.html",
+            "top",
+        ),
     ]
 
     # baton_summarize_fields = {
@@ -164,20 +244,24 @@ class NewsAdmin(ImportExportModelAdmin, TranslationAdmin):
     # }
 
     def get_category(self, instance):
-        return mark_safe('<span class="span-category-id-%d">%s</span>' % (instance.id, str(instance.category)))
-    get_category.short_description = 'category'
+        return mark_safe(
+            '<span class="span-category-id-%d">%s</span>'
+            % (instance.id, str(instance.category))
+        )
+
+    get_category.short_description = "category"
 
     def baton_cl_rows_attributes(self, request, cl):
         data = {}
         for news in cl.queryset.filter(category__id=2):
             data[news.id] = {
-                'class': 'table-info',
+                "class": "table-info",
                 # 'selector': '#result_list tr input[name=_selected_action][value=%d]' % news.id,
             }
         data[1] = {
-            'class': 'table-success',
-            'selector': '.span-category-id-%d' % 1,
-            'getParent': 'td',
-            'title': 'A fantasctic tooltip!'
+            "class": "table-success",
+            "selector": ".span-category-id-%d" % 1,
+            "getParent": "td",
+            "title": "A fantasctic tooltip!",
         }
         return json.dumps(data)
