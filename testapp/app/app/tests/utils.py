eb45e4228e11a448481729c3a10ca0d40ca18d40
abidibo <abidibo@gmail.com>
2025-05-21 17:53:55 +0200

    test: retry

--- testapp/app/app/tests/utils.py
+++ testapp/app/app/tests/utils.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 class element_has_css_class(object):
     """An expectation for checking that an element has a particular css class.
     locator - used to find the element
@@ -14,19 +15,20 @@ class element_has_css_class(object):
         self.css_class = css_class
 
     def __call__(self, driver):
-        element = driver.find_element(*self.locator)   # Finding the referenced element
+        element = driver.find_element(*self.locator)  # Finding the referenced element
         if self.css_class in element.get_attribute("class"):
             return element
         else:
             return False
 
+
 def make_driver():
     service = Service(ChromeDriverManager().install())
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(
         service=service,
         options=chrome_options,
